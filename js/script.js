35d97a44e17547b6c4e11a8328a6d96465262ebc
Yofre Ormaza <97921335+yofreee@users.noreply.github.com>
2022-11-28 22:45:59 -0500

    Actualizar archivos para página modo gym y pomodoro
    Como tal ya tiene el cronómetro ajustado para modo Gym que consta de solo un sonido al minuto por cada ejercicio y desde el minuto 45 se inicia el modo estudio, aplicando pomodoro 45/15 con un limite máximo de 8 horas

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ let btnIg = document.getElementById('iconIg');
 let btnYt = document.getElementById('iconYt');
 let btnSpty = document.getElementById('iconSpty');
 
+const btnGym = document.getElementById("modeGym");
+const btnStudy = document.getElementById("modeStudy");
+
+let gym = true;
+let study;
+
 
 let stopwatchInterval;
 let runningTime = 0;
@@ -43,16 +49,399 @@ const start = () => {
         stopwatch.textContent = calculateTime(runningTime);
     }, 1000)
 }
-
 const calculateTime = runningTime => {
     const total_seconds = Math.floor(runningTime / 1000);
     const total_minutes = Math.floor(total_seconds / 60);
+    const audioBell = document.getElementById('audioBell');
+    //if (total_seconds == 59) {
+    //    audioBell.play()
+    //} else {
+    //    audioBell.pause();
+    //    audioBell.currentTime = 0;
+    //}
 
     const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    //const display_secondsAlert = parseInt(display_seconds);
     const display_minutes = total_minutes.toString().padStart(2, "0");
-    
+    console.log(total_seconds);
+    const audioTictac = document.getElementById('audioTictac');
+
+    // Cronometro para modo Gym, suena en 1 minuto como maximo de un ejercicio
+    // El funcionamiento es sencillo, se temporiza por segundos. El segundo deseaso a ser el que active la campana del tiempo estimado tanto en modo gym como en modo estudio, debe ser restado en 6 para que concuerde con el setTimeout.
+    // En modo gym se estima 1 minuto por ejercicio
+    while (total_seconds === 60 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+            //audioBell.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+        }, 6000);
+        break;
+    }
+    // Cronometro para modo Pomodoro1 45/15 60minutos
+    while (total_seconds === 2700 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 3600 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+
+    // Cronometro para modo Pomodoro2 45/15 120minutos
+    while (total_seconds === 6300 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 7200 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 15000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+        Toast.fire({
+            icon: 'success',
+            title: 'Pomodoro 2: Logro diario desbloqueado ☺'
+        })
+
+        break;
+    }
+   
+    // Cronometro para modo Pomodoro3 45/15 180minutos
+    while (total_seconds === 9900 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 10800 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+
+    // Cronometro para modo Pomodoro4 45/15 240minutos
+    while (total_seconds === 13500 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 14400 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 15000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+        Toast.fire({
+            icon: 'success',
+            title: 'Pomodoro 4: Logro diario desbloqueado ☺'
+        })
+
+        break;
+    }
+   
+    // Cronometro para modo Pomodoro5 45/15 300minutos
+    while (total_seconds === 17100 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 18000 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+
+    // Cronometro para modo Pomodoro6 45/15 360minutos
+    while (total_seconds === 20700 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 21600 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 15000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+        Toast.fire({
+            icon: 'success',
+            title: 'Pomodoro 6: Logro diario desbloqueado ☺'
+        })
+
+        break;
+    }
+   
+    // Cronometro para modo Pomodoro7 45/15 420minutos
+    while (total_seconds === 24300 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 25200 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+
+    // Cronometro para modo Pomodoro8 45/15 480minutos
+    while (total_seconds === 27900 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Break de 15 minutos!',
+                showConfirmButton: false,
+                timer: 4000
+            })
+        }, 6000);
+        break;
+    }
+    while (total_seconds === 28800 - 6) {
+        audioTictac.currentTime = 0;
+        audioBell.currentTime = 0;
+        setTimeout(function sonidoMinutoUno() {
+            audioTictac.play();
+        }, 1000);
+        setTimeout(function sonidoNo(){
+            audioBell.play();
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '¡Fin del Break!',
+                showConfirmButton: false,
+                timer: 1000
+            })
+        }, 6000);
+        Swal.fire({
+            title: 'Buen trabajo!',
+            text: 'Para continuar debes reiniciar el cronómetro',
+            icon: 'success',
+            footer: '<p id="footer-8horas">¡Has llegado a tu meta diaria de concentración! Y espero que quieras continuar ☺</p>'
+        })
+        break;
+    }
+
     return `${display_minutes}:${display_seconds} `
 }
+
 
 // Eventos Click
 btnIg.addEventListener('click', function (){
